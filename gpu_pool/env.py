354688cd5496c836f70f55cb74f40f37db79570c
조창희 <79568286+hee000@users.noreply.github.com>
2023-12-14 21:16:57 +0900

    feat:gpu pool 구현 (#7)
    * feat: gpu pool 구현
    
    #6
    
    * docs: gpu pool readme 작성
    
    - 기존 readme에 수정/추가됨
    
    #6
    
    * docs: gpu pool readme 수정
    
    - usage consumer 주의사항 추가
    - message 양식 수정 (resize 인자)
    
    #6
    
    * fix: gpu pool database server 실행 로직 수정
    
    #6
    
    * fix: gpu pool mysql의 user, password를 환경변수로 변경
    
    #6
    
    * docs: gpu pool 환경변수 추가로 인한 readme 수정
    
    - utils.py는 설치 안내 print문 수정
    
    #6
    
    * refactor: gpu pool publisher, consumer 작업 주기 변경
    
    - 기존 10초에서 1분으로 변경
    
    #6
    
    * fix: gpu pool publisher, consumer 로직 수정
    
    - consumer가 pushed를 변경하도록 수정
    - consumer의 message pop, args 할당 로직 수정
    
    #6
    
    * fix: gpu pool push 수정
    
    #6
    
    * fix: gpu pool mysql query 버그 수정
    
    - error_message 테이블의 error_log 컬럼의 자료형 변경
    - error_log 관련  insert문 수정
    
    #6
    
    * refactor: gpu pool publisher 매직넘버 제거
    
    #6

---
+++ gpu_pool/env.py
@@ -0,0 +1,40 @@
+import os
+
+# 프로젝트 디렉토리 주소
+CWD = os.getcwd()
+
+# publisher의 ip와 port  
+PUBLISH_IP = 'ip_address' # ex)'10.28.224.65'
+PUBLISH_REDIS_PORT = 0 # ex) 30066
+PUBLISH_MYSQL_PORT = 0 # ex) 30067
+PUBLISH_MYSQL_USER = 'user'
+PUBLISH_MYSQL_PASSWORD = 'password'
+
+# redis 큐 이름
+QUEUE_NAME = 'ai_train_queue'
+
+# redis 큐에 푸시하려는 메시지 파일 주소
+MESSAGE_FILE_DIR = 'file_dir' # ex) './message.json' or f'{CWD}/message.json'
+
+# message.json 양식 예
+# {
+#   "branch": "develop",   // or commit ex) e6084c6b
+#   "name": "철수",
+#   "seed": "42",
+#   "epoch": "1",
+#   "dataset": "MaskBaseDataset",
+#   "augmentation": "BaseAugmentation",
+#   "resize": "128 96",
+#   "batch_size": "64",
+#   "valid_batch_size": "1000",
+#   "model": "BaseModel",
+#   "optimizer": "SGD",
+#   "lr": "1e-3",
+#   "val_ratio": "0.2",
+#   "criterion": "cross_entropy",
+#   "lr_decay_step": "20",
+#   "log_interval": "20",
+#   "patience": "5",
+#   "data_dir": "test_data_dir",
+#   "model_dir": "test_model_dir"
+# }
