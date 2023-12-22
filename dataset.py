64c358af595ef7389473dffe818a98c39f68e172
이재혁_T6132 <87365666+NewP1@users.noreply.github.com>
2023-12-22 12:31:24 +0900

    feat: multi label classfication 구현 (#15)
    * feat: multi label classfication 구현
    
    - train.py 수정
    - dataset.py 수정
    
    #13
    
    * feat: multilabel inference 추가
    
    - inference_multilabel.py 수정
    
    #13
    
    * feat: multilabel_train 함수 추가
    
    - train.py에서 train과 multilabel_train으로 나눔
    
    #13

--- dataset.py
+++ dataset.py
@@ -551,6 +551,96 @@ class TestDataset(Dataset):
         return len(self.img_paths)
 
 
+class MaskMultiLabelDataset(MaskBaseDataset):
+    num_classes = 3 + 2 + 3
+
+    def __getitem__(self, index):
+        """인덱스에 해당하는 데이터를 가져오는 메서드"""
+        assert self.transform is not None, ".set_tranform 메소드를 이용하여 transform 을 주입해주세요"
+
+        image = self.read_image(index)
+        mask_label = self.get_mask_label(index)
+        gender_label = self.get_gender_label(index)
+        age_label = self.get_age_label(index)
+
+        image_transform = self.transform(image)
+        return image_transform, mask_label, gender_label, age_label
+
+
+class MultiLabelSplitByProfileDataset(MaskBaseDataset):
+    num_classes = 3 + 2 + 3
+
+    def __init__(
+        self,
+        data_dir,
+        mean=(0.548, 0.504, 0.479),
+        std=(0.237, 0.247, 0.246),
+        val_ratio=0.2,
+    ):
+        self.indices = defaultdict(list)
+        super().__init__(data_dir, mean, std, val_ratio)
+
+    @staticmethod
+    def _split_profile(profiles, val_ratio):
+        """프로필을 학습과 검증용으로 나누는 메서드"""
+        length = len(profiles)
+        n_val = int(length * val_ratio)
+
+        val_indices = set(random.sample(range(length), k=n_val))
+        train_indices = set(range(length)) - val_indices
+        return {"train": train_indices, "val": val_indices}
+
+    def setup(self):
+        """데이터셋 설정을 하는 메서드. 프로필 기준으로 나눈다."""
+        profiles = os.listdir(self.data_dir)
+        profiles = [profile for profile in profiles if not profile.startswith(".")]
+        split_profiles = self._split_profile(profiles, self.val_ratio)
+
+        cnt = 0
+        for phase, indices in split_profiles.items():
+            for _idx in indices:
+                profile = profiles[_idx]
+                img_folder = os.path.join(self.data_dir, profile)
+                for file_name in os.listdir(img_folder):
+                    _file_name, ext = os.path.splitext(file_name)
+                    if (
+                        _file_name not in self._file_names
+                    ):  # "." 로 시작하는 파일 및 invalid 한 파일들은 무시합니다
+                        continue
+
+                    img_path = os.path.join(
+                        self.data_dir, profile, file_name
+                    )  # (resized_data, 000004_male_Asian_54, mask1.jpg)
+                    mask_label = self._file_names[_file_name]
+
+                    id, gender, race, age = profile.split("_")
+                    gender_label = GenderLabels.from_str(gender)
+                    age_label = AgeLabels.from_number(age)
+
+                    self.image_paths.append(img_path)
+                    self.mask_labels.append(mask_label)
+                    self.gender_labels.append(gender_label)
+                    self.age_labels.append(age_label)
+
+                    self.indices[phase].append(cnt)
+                    cnt += 1
+
+    def split_dataset(self) -> List[Subset]:
+        """프로필 기준으로 나눈 데이터셋을 Subset 리스트로 반환하는 메서드"""
+        return [Subset(self, indices) for phase, indices in self.indices.items()]
+
+    def __getitem__(self, index):
+        """인덱스에 해당하는 데이터를 가져오는 메서드"""
+        assert self.transform is not None, ".set_tranform 메소드를 이용하여 transform 을 주입해주세요"
+
+        image = self.read_image(index)
+        mask_label = self.get_mask_label(index)
+        gender_label = self.get_gender_label(index)
+        age_label = self.get_age_label(index)
+
+        image_transform = self.transform(image)
+        return image_transform, mask_label, gender_label, age_label
+
 class CustomDataset(Dataset):
     # 초기화 및 선언
     def __init__(self, data_dir: str) -> None:
