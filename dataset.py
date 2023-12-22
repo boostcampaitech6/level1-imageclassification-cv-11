a375141759262b3c933639b177131a54292e9ee4
_dlwogur_ <jaehyuk712@gmail.com>
2023-12-22 12:32:55 +0900

    feat: valid set 균형 만들기
    - dataset.py에 AugEqualDataset 추가
    
    #27

--- dataset.py
+++ dataset.py
@@ -8,6 +8,7 @@ from randaugment import RandAugment
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 from torchvision import transforms
+from collections import Counter
 
 import numpy as np
 import torch
@@ -455,6 +456,62 @@ class MaskBaseDataset(Dataset):
         train_set, val_set = random_split(self, [n_train, n_val])
         return train_set, val_set
 
+class AugEqualDataset(MaskBaseDataset):
+    class_labels = []
+    class_dict = {0:[], 1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[],
+        9:[], 10:[], 11:[], 12:[], 13:[], 14:[], 15:[], 16:[], 17:[]}
+    
+    def __init__(
+        self,
+        data_dir,
+        mean=(0.548, 0.504, 0.479),
+        std=(0.237, 0.247, 0.246),
+        val_ratio=0.2,
+    ):
+        super().__init__(data_dir, mean, std, val_ratio)
+    
+    def setup(self):
+        profiles = os.listdir(self.data_dir)
+        for profile in profiles:
+            if profile.startswith("."):  # "." 로 시작하는 파일은 무시합니다
+                continue
+
+            img_folder = os.path.join(self.data_dir, profile)
+            for file_name in os.listdir(img_folder):
+                _file_name, ext = os.path.splitext(file_name)
+                if (
+                    _file_name not in self._file_names
+                ):  # "." 로 시작하는 파일 및 invalid 한 파일들은 무시합니다
+                    continue
+
+                img_path = os.path.join(
+                    self.data_dir, profile, file_name
+                )  # (resized_data, 000004_male_Asian_54, mask1.jpg)
+                mask_label = self._file_names[_file_name]
+
+                id, gender, race, age = profile.split("_")
+                gender_label = GenderLabels.from_str(gender)
+                age_label = AgeLabels.from_number(age)
+
+                self.image_paths.append(img_path)
+                self.mask_labels.append(mask_label)
+                self.gender_labels.append(gender_label)
+                self.age_labels.append(age_label)
+                self.class_labels.append(self.encode_multi_class(mask_label, gender_label, age_label))
+
+        for i, c in enumerate(self.class_labels):
+            self.class_dict[c].append(i)
+    
+    def split_dataset(self) -> Tuple[Subset, Subset]:
+        min_cnt = Counter(self.class_labels).most_common()[-1][1]
+        n_per_class = int(min_cnt * .2)
+        val_indices = set()
+        for i in range(18):
+            val_indices = val_indices|set(random.sample(self.class_dict[i], k=n_per_class))
+        train_indices = set(range(len(self.class_labels))) - val_indices
+        train_set, val_set = Subset(self, list(train_indices)), Subset(self, list(val_indices))
+        return train_set, val_set
+
 
 class MaskSplitByProfileDataset(MaskBaseDataset):
     """
@@ -523,11 +580,6 @@ class MaskSplitByProfileDataset(MaskBaseDataset):
         return [Subset(self, indices) for phase, indices in self.indices.items()]
     
 class AgeRateProfileDataset(MaskBaseDataset):
-    """
-    train / val 나누는 기준을 이미지에 대해서 random 이 아닌 사람(profile)을 기준으로 나눕니다.
-    구현은 val_ratio 에 맞게 train / val 나누는 것을 이미지 전체가 아닌 사람(profile)에 대해서 진행하여 indexing 을 합니다.
-    이후 `split_dataset` 에서 index 에 맞게 Subset 으로 dataset 을 분기합니다.
-    """
 
     def __init__(
         self,
@@ -601,15 +653,9 @@ class AgeRateProfileDataset(MaskBaseDataset):
                     cnt += 1
 
     def split_dataset(self) -> List[Subset]:
-        """프로필 기준으로 나눈 데이터셋을 Subset 리스트로 반환하는 메서드"""
         return [Subset(self, indices) for phase, indices in self.indices.items()]
     
 class GenderAgeRateProfileDataset(MaskBaseDataset):
-    """
-    train / val 나누는 기준을 이미지에 대해서 random 이 아닌 사람(profile)을 기준으로 나눕니다.
-    구현은 val_ratio 에 맞게 train / val 나누는 것을 이미지 전체가 아닌 사람(profile)에 대해서 진행하여 indexing 을 합니다.
-    이후 `split_dataset` 에서 index 에 맞게 Subset 으로 dataset 을 분기합니다.
-    """
 
     def __init__(
         self,
