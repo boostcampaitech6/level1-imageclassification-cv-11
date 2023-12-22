bd60b1ef64197598c21f7c5752741ebef7c98e39
_dlwogur_ <jaehyuk712@gmail.com>
2023-12-22 12:32:55 +0900

    feat: age rate 고려한 dataset 구현
    - dataset.py에 AgeRateProfileDataset 추가
    
    #27

--- dataset.py
+++ dataset.py
@@ -521,6 +521,88 @@ class MaskSplitByProfileDataset(MaskBaseDataset):
     def split_dataset(self) -> List[Subset]:
         """프로필 기준으로 나눈 데이터셋을 Subset 리스트로 반환하는 메서드"""
         return [Subset(self, indices) for phase, indices in self.indices.items()]
+    
+class AgeRateProfileDataset(MaskBaseDataset):
+    """
+    train / val 나누는 기준을 이미지에 대해서 random 이 아닌 사람(profile)을 기준으로 나눕니다.
+    구현은 val_ratio 에 맞게 train / val 나누는 것을 이미지 전체가 아닌 사람(profile)에 대해서 진행하여 indexing 을 합니다.
+    이후 `split_dataset` 에서 index 에 맞게 Subset 으로 dataset 을 분기합니다.
+    """
+
+    def __init__(
+        self,
+        data_dir,
+        mean=(0.548, 0.504, 0.479),
+        std=(0.237, 0.247, 0.246),
+        val_ratio=0.2,
+    ):
+        self.indices = defaultdict(list)
+        super().__init__(data_dir, mean, std, val_ratio)
+
+    @staticmethod
+    def _split_profile(profiles, val_ratio):
+        length = len(profiles)
+        n_val = int(length * val_ratio)
+
+        young = []
+        middle = []
+        old = []
+
+        for i, profile in enumerate(profiles):
+            age = profile.split('_')[-1]
+            if age < 30:
+                young.append(i)
+            elif age < 60:
+                middle.append(i)
+            else:
+                old.append(i)
+
+        y_indices = set(random.sample(young, k=256))
+        m_indices = set(random.sample(middle, k=245))
+        o_indices = set(random.sample(old, k=39))
+
+        val_indices = y_indices|m_indices|o_indices
+        train_indices = set(range(length)) - val_indices
+        return {"train": train_indices, "val": val_indices}
+
+    def setup(self):
+        """데이터셋 설정을 하는 메서드. 프로필 기준으로 나눈다."""
+        profiles = os.listdir(self.data_dir)
+        profiles = [profile for profile in profiles if not profile.startswith(".")]
+        split_profiles = self._split_profile(profiles, self.val_ratio)
+
+        cnt = 0
+        for phase, indices in split_profiles.items():
+            for _idx in indices:
+                profile = profiles[_idx]
+                img_folder = os.path.join(self.data_dir, profile)
+                for file_name in os.listdir(img_folder):
+                    _file_name, ext = os.path.splitext(file_name)
+                    if (
+                        _file_name not in self._file_names
+                    ):  # "." 로 시작하는 파일 및 invalid 한 파일들은 무시합니다
+                        continue
+
+                    img_path = os.path.join(
+                        self.data_dir, profile, file_name
+                    )  # (resized_data, 000004_male_Asian_54, mask1.jpg)
+                    mask_label = self._file_names[_file_name]
+
+                    id, gender, race, age = profile.split("_")
+                    gender_label = GenderLabels.from_str(gender)
+                    age_label = AgeLabels.from_number(age)
+
+                    self.image_paths.append(img_path)
+                    self.mask_labels.append(mask_label)
+                    self.gender_labels.append(gender_label)
+                    self.age_labels.append(age_label)
+
+                    self.indices[phase].append(cnt)
+                    cnt += 1
+
+    def split_dataset(self) -> List[Subset]:
+        """프로필 기준으로 나눈 데이터셋을 Subset 리스트로 반환하는 메서드"""
+        return [Subset(self, indices) for phase, indices in self.indices.items()]
 
 
 class TestDataset(Dataset):
