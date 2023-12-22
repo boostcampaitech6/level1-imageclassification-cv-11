a6b4d1c865cdefedded46deea946bd5ef3decd72
이재혁_T6132 <87365666+NewP1@users.noreply.github.com>
2023-12-22 13:07:52 +0900

    feat: 비율 고려한 dataset class 구현 (#31)
    * feat: age rate 고려한 dataset 구현
    
    - dataset.py에 AgeRateProfileDataset 추가
    
    #27
    
    * fix: AgeRateProfileDataset age type 수정
    
    #27
    
    * feat: gender, age 고려한 dataset 구현
    
    - dataset.py 에 GenderAgeRateProfileDataset 함수 추가
    
    #27
    
    * feat: valid set 균형 만들기
    
    - dataset.py에 AugEqualDataset 추가
    
    #27
    
    * feat: 데이터 수 증대 구현
    
    - 전체 데이터에 centercrop한 이미지를 추가
    - age label old에 colorjitter를 적용한 이미지를 추가
    
    #27

--- dataset.py
+++ dataset.py
@@ -8,6 +8,7 @@ from randaugment import RandAugment
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 from torchvision import transforms
+from collections import Counter
 
 import numpy as np
 import torch
@@ -456,6 +457,136 @@ class MaskBaseDataset(Dataset):
         return train_set, val_set
 
 
+
+
+class AugmentationType(int, Enum):
+    NONE = -1
+    DOUBLE = 0
+    OLDAGE = 1
+    DOUBLE_OLDAGE = 2
+
+class AugEqualDataset(MaskBaseDataset):
+    agument_check = []
+    class_labels = []
+    class_dict = {0:[], 1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[],
+        9:[], 10:[], 11:[], 12:[], 13:[], 14:[], 15:[], 16:[], 17:[]}
+    
+    def __init__(
+        self,
+        data_dir,
+        mean=(0.548, 0.504, 0.479),
+        std=(0.237, 0.247, 0.246),
+        val_ratio=0.2,
+    ):
+        super().__init__(data_dir, mean, std, val_ratio)
+        resize = (299, 299)
+        self.aug_transform = [
+                Compose( # double
+                    [
+                        CenterCrop((320, 256)),
+                        Resize(resize, Image.BILINEAR),
+                        ToTensor(),
+                        Normalize(mean=mean, std=std),
+                    ]
+                ),
+                Compose( # oldage
+                    [
+                        Resize((299, 299), Image.BILINEAR),
+                        ColorJitter(0.5, 0.5, 0.5, 0.5),
+                        ToTensor(),
+                        Normalize(mean=mean, std=std),
+                    ]
+                ),
+                Compose( # double_oldage
+                    [
+                        CenterCrop((320, 256)),
+                        Resize((299, 299), Image.BILINEAR),
+                        ColorJitter(0.5, 0.5, 0.5, 0.5),
+                        ToTensor(),
+                        Normalize(mean=mean, std=std),
+                    ]
+                )
+            ]
+    
+    def setup(self):
+        profiles = os.listdir(self.data_dir)
+        for profile in profiles:
+            if profile.startswith("."):  # "." 로 시작하는 파일은 무시합니다
+                continue
+
+            img_folder = os.path.join(self.data_dir, profile)
+            for file_name in os.listdir(img_folder):
+                _file_name, ext = os.path.splitext(file_name)
+                if (
+                    _file_name not in self._file_names
+                ):  # "." 로 시작하는 파일 및 invalid 한 파일들은 무시합니다
+                    continue
+
+                img_path = os.path.join(
+                    self.data_dir, profile, file_name
+                )  # (resized_data, 000004_male_Asian_54, mask1.jpg)
+                mask_label = self._file_names[_file_name]
+
+                id, gender, race, age = profile.split("_")
+                gender_label = GenderLabels.from_str(gender)
+                age_label = AgeLabels.from_number(age)
+
+                class_label = self.encode_multi_class(mask_label, gender_label, age_label)
+
+                for aug_type in range(-1, 1):
+                    self.agument_check.append(AugmentationType(aug_type))
+                    self.image_paths.append(img_path)
+                    self.mask_labels.append(mask_label)
+                    self.gender_labels.append(gender_label)
+                    self.age_labels.append(age_label)
+                    self.class_labels.append(class_label)
+                    
+                if age_label == AgeLabels.OLD:
+                    for _ in range(5):
+                        for aug_type_age in range(1, 3):
+                            self.agument_check.append(AugmentationType(aug_type_age))
+                            self.image_paths.append(img_path)
+                            self.mask_labels.append(mask_label)
+                            self.gender_labels.append(gender_label)
+                            self.age_labels.append(age_label)
+                            self.class_labels.append(class_label)
+
+        for i, c in enumerate(self.class_labels):
+            self.class_dict[c].append(i)
+
+    def get_old_age_agument(self, index) -> AugmentationType:
+        return self.agument_check[index]
+
+    def __getitem__(self, index):
+        """인덱스에 해당하는 데이터를 가져오는 메서드"""
+        assert self.transform is not None, ".set_tranform 메소드를 이용하여 transform 을 주입해주세요"
+
+        image = self.read_image(index)
+        mask_label = self.get_mask_label(index)
+        gender_label = self.get_gender_label(index)
+        age_label = self.get_age_label(index)
+        multi_class_label = self.encode_multi_class(mask_label, gender_label, age_label)
+
+        old_age_agument = self.get_old_age_agument(index)
+        
+        if old_age_agument != AugmentationType.NONE:
+            image_transform = self.aug_transform[old_age_agument](image)
+        else:
+            image_transform = self.transform(image)
+
+        return image_transform, multi_class_label
+    
+    def split_dataset(self) -> Tuple[Subset, Subset]:
+        min_cnt = Counter(self.class_labels).most_common()[-1][1]
+        n_per_class = int(min_cnt * .2)
+        val_indices = set()
+        for i in range(18):
+            val_indices = val_indices|set(random.sample(self.class_dict[i], k=n_per_class))
+        train_indices = set(range(len(self.class_labels))) - val_indices
+        train_set, val_set = Subset(self, list(train_indices)), Subset(self, list(val_indices))
+        return train_set, val_set
+
+
 class MaskSplitByProfileDataset(MaskBaseDataset):
     """
     train / val 나누는 기준을 이미지에 대해서 random 이 아닌 사람(profile)을 기준으로 나눕니다.
@@ -480,6 +611,174 @@ class MaskSplitByProfileDataset(MaskBaseDataset):
         n_val = int(length * val_ratio)
 
         val_indices = set(random.sample(range(length), k=n_val))
+        train_indices = set(range(length)) - val_indices
+        return {"train": train_indices, "val": val_indices}
+
+    def setup(self):
+        """데이터셋 설정을 하는 메서드. 프로필 기준으로 나눈다."""
+        profiles = os.listdir(self.data_dir)
+        profiles = [profile for profile in profiles if not profile.startswith(".")]
+        split_profiles = self._split_profile(profiles, self.val_ratio)
+
+        cnt = 0
+        for phase, indices in split_profiles.items():
+            for _idx in indices:
+                profile = profiles[_idx]
+                img_folder = os.path.join(self.data_dir, profile)
+                for file_name in os.listdir(img_folder):
+                    _file_name, ext = os.path.splitext(file_name)
+                    if (
+                        _file_name not in self._file_names
+                    ):  # "." 로 시작하는 파일 및 invalid 한 파일들은 무시합니다
+                        continue
+
+                    img_path = os.path.join(
+                        self.data_dir, profile, file_name
+                    )  # (resized_data, 000004_male_Asian_54, mask1.jpg)
+                    mask_label = self._file_names[_file_name]
+
+                    id, gender, race, age = profile.split("_")
+                    gender_label = GenderLabels.from_str(gender)
+                    age_label = AgeLabels.from_number(age)
+
+                    self.image_paths.append(img_path)
+                    self.mask_labels.append(mask_label)
+                    self.gender_labels.append(gender_label)
+                    self.age_labels.append(age_label)
+
+                    self.indices[phase].append(cnt)
+                    cnt += 1
+
+    def split_dataset(self) -> List[Subset]:
+        """프로필 기준으로 나눈 데이터셋을 Subset 리스트로 반환하는 메서드"""
+        return [Subset(self, indices) for phase, indices in self.indices.items()]
+    
+class AgeRateProfileDataset(MaskBaseDataset):
+
+    def __init__(
+        self,
+        data_dir,
+        mean=(0.548, 0.504, 0.479),
+        std=(0.237, 0.247, 0.246),
+        val_ratio=0.2,
+    ):
+        self.indices = defaultdict(list)
+        super().__init__(data_dir, mean, std, val_ratio)
+
+    @staticmethod
+    def _split_profile(profiles, val_ratio):
+        length = len(profiles)
+        n_val = int(length * val_ratio)
+
+        young = []
+        middle = []
+        old = []
+
+        for i, profile in enumerate(profiles):
+            age = int(profile.split('_')[-1])
+            if age < 30:
+                young.append(i)
+            elif age < 60:
+                middle.append(i)
+            else:
+                old.append(i)
+
+        y_indices = set(random.sample(young, k=256))
+        m_indices = set(random.sample(middle, k=245))
+        o_indices = set(random.sample(old, k=39))
+
+        val_indices = y_indices|m_indices|o_indices
+        train_indices = set(range(length)) - val_indices
+        return {"train": train_indices, "val": val_indices}
+
+    def setup(self):
+        """데이터셋 설정을 하는 메서드. 프로필 기준으로 나눈다."""
+        profiles = os.listdir(self.data_dir)
+        profiles = [profile for profile in profiles if not profile.startswith(".")]
+        split_profiles = self._split_profile(profiles, self.val_ratio)
+
+        cnt = 0
+        for phase, indices in split_profiles.items():
+            for _idx in indices:
+                profile = profiles[_idx]
+                img_folder = os.path.join(self.data_dir, profile)
+                for file_name in os.listdir(img_folder):
+                    _file_name, ext = os.path.splitext(file_name)
+                    if (
+                        _file_name not in self._file_names
+                    ):  # "." 로 시작하는 파일 및 invalid 한 파일들은 무시합니다
+                        continue
+
+                    img_path = os.path.join(
+                        self.data_dir, profile, file_name
+                    )  # (resized_data, 000004_male_Asian_54, mask1.jpg)
+                    mask_label = self._file_names[_file_name]
+
+                    id, gender, race, age = profile.split("_")
+                    gender_label = GenderLabels.from_str(gender)
+                    age_label = AgeLabels.from_number(age)
+
+                    self.image_paths.append(img_path)
+                    self.mask_labels.append(mask_label)
+                    self.gender_labels.append(gender_label)
+                    self.age_labels.append(age_label)
+
+                    self.indices[phase].append(cnt)
+                    cnt += 1
+
+    def split_dataset(self) -> List[Subset]:
+        return [Subset(self, indices) for phase, indices in self.indices.items()]
+    
+class GenderAgeRateProfileDataset(MaskBaseDataset):
+
+    def __init__(
+        self,
+        data_dir,
+        mean=(0.548, 0.504, 0.479),
+        std=(0.237, 0.247, 0.246),
+        val_ratio=0.2,
+    ):
+        self.indices = defaultdict(list)
+        super().__init__(data_dir, mean, std, val_ratio)
+
+    @staticmethod
+    def _split_profile(profiles, val_ratio):
+        length = len(profiles)
+        n_val = int(length * val_ratio)
+
+        young_female = []
+        young_male = []
+        middle_female = []
+        middle_male = []
+        old_female = []
+        old_male = []
+        name = [young_female, young_male, middle_female, middle_male, old_female, old_male]
+        for i, profile in enumerate(profiles):
+            id, gender, race, age = profile.split('_')
+            if int(age) < 30:
+                if gender == 'male':
+                    young_male.append(i)
+                else:
+                    young_female.append(i)
+            elif int(age) < 60:
+                if gender == 'male':
+                    middle_male.append(i)
+                else:
+                    middle_female.append(i)
+            else:
+                if gender == 'male':
+                    old_male.append(i)
+                else:
+                    old_female.append(i)
+
+        yf = set(random.sample(young_female, k=int(256*.61)))
+        ym = set(random.sample(young_male, k=int(256*.39)+1))
+        mf = set(random.sample(middle_female, k=int(245*.61)))
+        mm = set(random.sample(middle_male, k=int(245*.39)+1))
+        of = set(random.sample(old_female, k=int(39*.61)))
+        om = set(random.sample(old_male, k=int(39*.39)+1))
+
+        val_indices = yf|ym|mf|mm|of|om
         train_indices = set(range(length)) - val_indices
         return {"train": train_indices, "val": val_indices}
 
