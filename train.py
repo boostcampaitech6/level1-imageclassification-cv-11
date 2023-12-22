dc8103de924a57240989f73be5d9b4b0be503954
_dlwogur_ <jaehyuk712@gmail.com>
2023-12-22 12:28:42 +0900

    feat: multilabel_train 함수 추가
    - train.py에서 train과 multilabel_train으로 나눔

--- train.py
+++ train.py
@@ -198,61 +198,16 @@ def train(data_dir, model_dir, args):
         loss_value = 0
         matches = 0
         for idx, train_batch in enumerate(train_loader):
-            loss = 0
-            preds = 0
-            if args.dataset in [
-                "MaskMultiLabelDataset",
-                "MultiLabelSplitByProfileDataset",
-            ]:
-                inputs, mask_labels, gender_labels, age_labels = train_batch
-                inputs = inputs.to(device)
-                mask_labels = mask_labels.to(device)
-                gender_labels = gender_labels.to(device)
-                age_labels = age_labels.to(device)
-                labels = torch.Tensor(
-                    [
-                        [
-                            k
-                            for k, v in classes.items()
-                            if v == [mask_labels[i], gender_labels[i], age_labels[i]]
-                        ]
-                        for i in range(args.batch_size)
-                    ]
-                )
-                optimizer.zero_grad()
-                outs = model(inputs)
-                mask_outs, gender_outs, age_outs = torch.split(outs, [3, 2, 3], dim=1)
-                mask_preds, gender_preds, age_preds = (
-                    torch.argmax(mask_outs, dim=-1),
-                    torch.argmax(gender_outs, dim=-1),
-                    torch.argmax(age_outs, dim=-1),
-                )
-                preds = torch.Tensor(
-                    [
-                        [
-                            k
-                            for k, v in classes.items()
-                            if v == [mask_preds[i], gender_preds[i], age_preds[i]]
-                        ]
-                        for i in range(args.batch_size)
-                    ]
-                )
 
-                mask_loss = criterion(mask_outs, mask_labels)
-                gender_loss = criterion(gender_outs, gender_labels)
-                age_loss = criterion(age_outs, age_labels)
+            inputs, labels = train_batch
+            inputs = inputs.to(device)
+            labels = labels.to(device)
 
-                loss = mask_loss + gender_loss + age_loss
-            else:
-                inputs, labels = train_batch
-                inputs = inputs.to(device)
-                labels = labels.to(device)
+            optimizer.zero_grad()
 
-                optimizer.zero_grad()
-
-                outs = model(inputs)
-                preds = torch.argmax(outs, dim=-1)
-                loss = criterion(outs, labels)
+            outs = model(inputs)
+            preds = torch.argmax(outs, dim=-1)
+            loss = criterion(outs, labels)
 
             loss.backward()
             optimizer.step()
@@ -289,62 +244,15 @@ def train(data_dir, model_dir, args):
             all_labels = []
             all_preds = []
             for val_batch in val_loader:
-                loss_item = 0
-                preds = 0
-                if args.dataset in [
-                    "MaskMultiLabelDataset",
-                    "MultiLabelSplitByProfileDataset",
-                ]:
-                    inputs, mask_labels, gender_labels, age_labels = val_batch
-                    inputs = inputs.to(device)
-                    mask_labels = mask_labels.to(device)
-                    gender_labels = gender_labels.to(device)
-                    age_labels = age_labels.to(device)
-                    labels = torch.Tensor(
-                        [
-                            [
-                                k
-                                for k, v in classes.items()
-                                if v
-                                == [mask_labels[i], gender_labels[i], age_labels[i]]
-                            ]
-                            for i in range(args.valid_batch_size)
-                        ]
-                    )
-                    outs = model(inputs)
-                    mask_outs, gender_outs, age_outs = torch.split(
-                        outs, [3, 2, 3], dim=1
-                    )
-                    mask_preds, gender_preds, age_preds = (
-                        torch.argmax(mask_outs, dim=-1),
-                        torch.argmax(gender_outs, dim=-1),
-                        torch.argmax(age_outs, dim=-1),
-                    )
-                    preds = torch.Tensor(
-                        [
-                            [
-                                k
-                                for k, v in classes.items()
-                                if v == [mask_preds[i], gender_preds[i], age_preds[i]]
-                            ]
-                            for i in range(args.valid_batch_size)
-                        ]
-                    )
 
-                    mask_loss_item = criterion(mask_outs, mask_labels).item()
-                    gender_loss_item = criterion(gender_outs, gender_labels).item()
-                    age_loss_item = criterion(age_outs, age_labels).item()
+                inputs, labels = val_batch
+                inputs = inputs.to(device)
+                labels = labels.to(device)
 
-                    loss_item = mask_loss_item + gender_loss_item + age_loss_item
-                else:
-                    inputs, labels = val_batch
-                    inputs = inputs.to(device)
-                    labels = labels.to(device)
+                outs = model(inputs)
+                preds = torch.argmax(outs, dim=-1)
 
-                    outs = model(inputs)
-                    preds = torch.argmax(outs, dim=-1)
-
-                    loss_item = criterion(outs, labels).item()
+                loss_item = criterion(outs, labels).item()
                     
                 acc_item = (labels == preds).sum().item()
                 val_loss_items.append(loss_item)
@@ -365,6 +273,269 @@ def train(data_dir, model_dir, args):
                         preds,
                         n=16,
                         shuffle=args.dataset != "MaskSplitByProfileDataset",
+                    )
+
+            val_loss = np.sum(val_loss_items) / len(val_loader)
+            val_acc = np.sum(val_acc_items) / len(val_set)
+            val_f1 = f1_score(all_labels, all_preds, average='macro')
+
+            iteration_change_loss += 1
+            if val_acc > best_val_acc:
+                print(
+                    f"New best model for val accuracy : {val_acc:4.2%}! saving the best model.."
+                )
+                torch.save(model.module.state_dict(), f"{save_dir}/best.pth")
+                best_val_acc = val_acc
+
+            if val_loss < best_val_loss:
+                best_val_loss = val_loss
+                iteration_change_loss = 0
+            
+            torch.save(model.module.state_dict(), f"{save_dir}/last.pth")
+            print(
+                f"[Val] acc : {val_acc:4.2%}, loss: {val_loss:4.2} || "
+                f"best acc : {best_val_acc:4.2%}, best loss: {best_val_loss:4.2}"
+            )
+            logger.add_scalar("Val/loss", val_loss, epoch)
+            logger.add_scalar("Val/accuracy", val_acc, epoch)
+            logger.add_scalar("Val/f1_score", val_f1, epoch)
+            logger.add_figure("results", figure, epoch)
+            wandb.log({
+                "Train/loss": train_loss, 
+                "Train/accuracy": train_acc,
+                "Val/loss": val_loss, 
+                "Val/accuracy": val_acc,
+                "Val/f1_score": val_f1,
+                "results": wandb.Image(figure)
+            }, step=epoch)
+            print()
+
+        if iteration_change_loss == args.patience:
+            print(f'Early stopping after {iteration_change_loss} iterations without the increase of the val loss.')
+            break
+
+def multilabel_train(data_dir, model_dir, args):
+    seed_everything(args.seed)
+
+    for folder_name in os.listdir(args.model_dir):
+        subfolder_path = os.path.join(args.model_dir, folder_name)
+        shutil.rmtree(subfolder_path)
+
+    wandb.run.name = f"{args.camper_id}-{args.name}"
+    save_dir = increment_path(os.path.join(model_dir, f"{args.camper_id}-{args.name}"))
+
+    # -- settings
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    # -- dataset
+    dataset_module = getattr(
+        import_module("dataset"), args.dataset
+    )
+    dataset = dataset_module(
+        data_dir=data_dir,
+    )
+    num_classes = dataset.num_classes  # 18
+
+    # -- augmentation
+    transform_module = getattr(
+        import_module("dataset"), args.augmentation
+    )
+    transform = transform_module(
+        resize=args.resize,
+        mean=dataset.mean,
+        std=dataset.std,
+    )
+    dataset.set_transform(transform)
+
+    # -- data_loader
+    train_set, val_set = dataset.split_dataset()
+
+    train_loader = DataLoader(
+        train_set,
+        batch_size=args.batch_size,
+        num_workers=multiprocessing.cpu_count() // 2,
+        shuffle=True,
+        pin_memory=use_cuda,
+        drop_last=True,
+    )
+
+    val_loader = DataLoader(
+        val_set,
+        batch_size=args.valid_batch_size,
+        num_workers=multiprocessing.cpu_count() // 2,
+        shuffle=False,
+        pin_memory=use_cuda,
+        drop_last=True,
+    )
+
+    # -- model
+    model_module = getattr(import_module("model"), args.model)
+    model = model_module(num_classes=num_classes).to(device)
+    model = torch.nn.DataParallel(model)
+
+    # -- loss & metric
+    criterion = create_criterion(args.criterion)  # default: cross_entropy
+    opt_module = getattr(import_module("torch.optim"), args.optimizer)  # default: SGD
+    optimizer = opt_module(
+        filter(lambda p: p.requires_grad, model.parameters()),
+        lr=args.lr,
+        weight_decay=5e-4,
+    )
+    scheduler = StepLR(optimizer, args.lr_decay_step, gamma=0.5)
+
+    # -- logging
+    logger = SummaryWriter(log_dir=save_dir)
+    with open(os.path.join(save_dir, "config.json"), "w", encoding="utf-8") as f:
+        json.dump(vars(args), f, ensure_ascii=False, indent=4)
+
+    best_val_acc = 0
+    best_val_loss = np.inf
+    
+    iteration_change_loss = 0
+    for epoch in range(args.epochs):
+        # train loop
+        model.train()
+        loss_value = 0
+        matches = 0
+        for idx, train_batch in enumerate(train_loader):
+            inputs, mask_labels, gender_labels, age_labels = train_batch
+            inputs = inputs.to(device)
+            mask_labels = mask_labels.to(device)
+            gender_labels = gender_labels.to(device)
+            age_labels = age_labels.to(device)
+            labels = torch.Tensor(
+                [
+                    [
+                        k
+                        for k, v in classes.items()
+                        if v == [mask_labels[i], gender_labels[i], age_labels[i]]
+                    ]
+                    for i in range(args.batch_size)
+                ]
+            )
+            optimizer.zero_grad()
+            outs = model(inputs)
+            mask_outs, gender_outs, age_outs = torch.split(outs, [3, 2, 3], dim=1)
+            mask_preds, gender_preds, age_preds = (
+                torch.argmax(mask_outs, dim=-1),
+                torch.argmax(gender_outs, dim=-1),
+                torch.argmax(age_outs, dim=-1),
+            )
+            preds = torch.Tensor(
+                [
+                    [
+                        k
+                        for k, v in classes.items()
+                        if v == [mask_preds[i], gender_preds[i], age_preds[i]]
+                    ]
+                    for i in range(args.batch_size)
+                ]
+            )
+
+            mask_loss = criterion(mask_outs, mask_labels)
+            gender_loss = criterion(gender_outs, gender_labels)
+            age_loss = criterion(age_outs, age_labels)
+
+            loss = mask_loss + gender_loss + age_loss
+
+            loss.backward()
+            optimizer.step()
+
+            loss_value += loss.item()
+            matches += (preds == labels).sum().item()
+            if (idx + 1) % args.log_interval == 0:
+                train_loss = loss_value / args.log_interval
+                train_acc = matches / args.batch_size / args.log_interval
+                current_lr = get_lr(optimizer)
+                print(
+                    f"Epoch[{epoch}/{args.epochs}]({idx + 1}/{len(train_loader)}) || "
+                    f"training loss {train_loss:4.4} || training accuracy {train_acc:4.2%} || lr {current_lr}"
+                )
+                logger.add_scalar(
+                    "Train/loss", train_loss, epoch * len(train_loader) + idx
+                )
+                logger.add_scalar(
+                    "Train/accuracy", train_acc, epoch * len(train_loader) + idx
+                )
+
+                loss_value = 0
+                matches = 0
+
+        scheduler.step()
+
+        # val loop
+        with torch.no_grad():
+            print("Calculating validation results...")
+            model.eval()
+            val_loss_items = []
+            val_acc_items = []
+            figure = None
+            all_labels = []
+            all_preds = []
+            for val_batch in val_loader:
+                inputs, mask_labels, gender_labels, age_labels = val_batch
+                inputs = inputs.to(device)
+                mask_labels = mask_labels.to(device)
+                gender_labels = gender_labels.to(device)
+                age_labels = age_labels.to(device)
+                labels = torch.Tensor(
+                    [
+                        [
+                            k
+                            for k, v in classes.items()
+                            if v
+                            == [mask_labels[i], gender_labels[i], age_labels[i]]
+                        ]
+                        for i in range(args.valid_batch_size)
+                    ]
+                )
+                outs = model(inputs)
+                mask_outs, gender_outs, age_outs = torch.split(
+                    outs, [3, 2, 3], dim=1
+                )
+                mask_preds, gender_preds, age_preds = (
+                    torch.argmax(mask_outs, dim=-1),
+                    torch.argmax(gender_outs, dim=-1),
+                    torch.argmax(age_outs, dim=-1),
+                )
+                preds = torch.Tensor(
+                    [
+                        [
+                            k
+                            for k, v in classes.items()
+                            if v == [mask_preds[i], gender_preds[i], age_preds[i]]
+                        ]
+                        for i in range(args.valid_batch_size)
+                    ]
+                )
+
+                mask_loss_item = criterion(mask_outs, mask_labels).item()
+                gender_loss_item = criterion(gender_outs, gender_labels).item()
+                age_loss_item = criterion(age_outs, age_labels).item()
+
+                loss_item = mask_loss_item + gender_loss_item + age_loss_item
+
+                    
+                acc_item = (labels == preds).sum().item()
+                val_loss_items.append(loss_item)
+                val_acc_items.append(acc_item)
+                all_labels.extend(labels.cpu().numpy())
+                all_preds.extend(preds.cpu().numpy())
+
+                if figure is None:
+                    inputs_np = (
+                        torch.clone(inputs).detach().cpu().permute(0, 2, 3, 1).numpy()
+                    )
+                    inputs_np = dataset_module.denormalize_image(
+                        inputs_np, dataset.mean, dataset.std
+                    )
+                    figure = grid_image(
+                        inputs_np,
+                        labels,
+                        preds,
+                        n=16,
+                        shuffle=args.dataset != "MultiLabelSplitByProfileDataset",
                     )
 
             val_loss = np.sum(val_loss_items) / len(val_loader)
@@ -512,5 +683,8 @@ if __name__ == "__main__":
     model_dir = args.model_dir
 
     wandb.init(project="level1-imageclassification-cv-11", entity="cv-11", config=args)
-    train(data_dir, model_dir, args)
+    if args.dataset in ['MultiLabelSplitByProfileDataset','MaskMultiLabelDataset']:
+        multilabel_train(data_dir, model_dir, args)
+    else:
+        train(data_dir, model_dir, args)
     wandb.finish()
